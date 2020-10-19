2e41342b5a5e3536f94582c9f6a176c5f78a7c0e
Pavan <pavantejakusunuri@gmail.com>
2020-10-19 21:40:50 +0530

    Added Custom error message for Error messages

---
+++ 01.Form-Validator/script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//  Show input Error Messages
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//  Event Listeners
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(usename.value)
+})
